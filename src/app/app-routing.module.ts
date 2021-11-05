cab31ae58061bd371ea5a9af8bdfcc09e1e6a2e9
Khyzhniak <v.khyzhniak@pin-up.team>
2021-11-05 16:00:32 +0200

    all pages added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -32,7 +32,11 @@ const routes: Routes = [
     canActivate: [AuthGuard],
     component: SiteLayoutComponent,
     children: [
-      { path: 'overview', loadChildren: () => import('./+overview/overview.module').then(m => m.OverviewModule) }
+      { path: 'overview', loadChildren: () => import('./+overview/overview.module').then(m => m.OverviewModule) },
+      { path: 'analytics', loadChildren: () => import('./+analytics/analytics.module').then(m => m.AnalyticsModule) },
+      { path: 'history', loadChildren: () => import('./+history/history.module').then(m => m.HistoryModule) },
+      { path: 'order', loadChildren: () => import('./+order/order.module').then(m => m.OrderModule) },
+      { path: 'categories', loadChildren: () => import('./+categories/categories.module').then(m => m.CategoriesModule) },
     ]
   }
 ];
@@ -41,4 +45,4 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
