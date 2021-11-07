9c8c8c367ca870dac8c5b23ebf1a591f1e77ed51
viktorkhyzhniak <viktor.khyshnyak@gmail.com>
2021-11-07 11:28:48 +0200

    added google experiment dataLayer

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>\n',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit{
+  public ngOnInit() {
+    window['dataLayer'].push({'event': 'optimize.activate'});
+  }
+}
