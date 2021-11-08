b7b1b42990256245e2785abb2c1bec6cd9154012
Khyzhniak <v.khyzhniak@pin-up.team>
2021-11-08 13:19:55 +0200

    fake cookie aded

--- src/app/+history/history-page/history-page.component.ts
+++ src/app/+history/history-page/history-page.component.ts
@@ -10,4 +10,12 @@ export class HistoryPageComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {}
+
+  setFakeCookie() {
+    var date = new Date();
+    date.setTime(date.getTime() + (24*60*60*1000));
+    const expires = "; expires=" + date.toUTCString();
+
+    document.cookie = 'experiment' + "=" + ('true')  + expires + "; path=/";
+  }
 }
