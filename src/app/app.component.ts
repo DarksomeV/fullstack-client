dd411ff21e75c54e9511ea407bc261ab810762c6
Khyzhniak <v.khyzhniak@pin-up.team>
2021-11-08 16:01:10 +0200

    changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,25 @@
 import {Component, OnInit} from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+declare var ga: Function;
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>\n',
 })
 export class AppComponent implements OnInit{
+  constructor(private _router: Router) {
+  }
   public ngOnInit() {
     console.log(window['dataLayer']);
     window['dataLayer'].push({'event': 'optimize.activate'});
+    this._router.events
+      .pipe(
+        filter(event  => event instanceof NavigationEnd)
+      )
+      .subscribe((event: NavigationEnd) => {
+      ga('set', 'page', event.urlAfterRedirects);
+      ga('send', 'pageview')
+    });
   }
 }
